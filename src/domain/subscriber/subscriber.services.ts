5d3554c8eb682e4d8df6368687dec18f367a0f93
Eid Mohammad Pouya <empouya03@gmail.com>
2026-01-14 22:01:00 +0100

    fix: modify subscriber related domain

--- src/domain/subscriber/subscriber.services.ts
+++ src/domain/subscriber/subscriber.services.ts
@@ -1,6 +1,10 @@
 import { SubscriberRepository } from '@/repositories/subscriber.repository';
 
 export const SubscriberService = {
+  async getAll() {
+    return SubscriberRepository.findAll();
+  },
+
   async subscribe(email: string) {
     const existing = await SubscriberRepository.findByEmail(email);
     if (existing) {
